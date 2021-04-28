d000f76bf29a6592d5cf1e224a41018772a13f93
benpemberton <benedictpemberton@gmail.com>
2021-04-28 12:56:32 +0000

    Have started. Trying to resolve dynamic variable naming.

---
+++ script.js
@@ -0,0 +1,31 @@
+let myLibrary = [];
+
+function Book(title, author, genre, pages, read) {
+    this.title = title
+    this.author = author
+    this.genre = genre
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+    const details = [];
+
+    details[0] = prompt('what is the name of the book?', '');
+    details[1] = prompt('who is the author?', '');
+    details[2] = prompt('what is the genre?', '');
+    details[3] = prompt('how many pages is it?', '');
+    details[4] = prompt('whave you read it yet?', '');
+
+    let book = new Book(...details);
+
+    myLibrary.push(book);
+    
+    if (myLibrary.length > 1) {
+        () => {
+            
+        }
+    }
+}
+
+addBookToLibrary();
