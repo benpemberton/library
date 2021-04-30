04e6cb8cfe8d2265b4fb8392a2d27c7ddc6c3bd7
Ben Pemberton <benedictpemberton@gmail.com>
2021-05-01 00:23:43 +0100

    Added remove and toggle read status buttons. Need to sort indexing after remove.

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const newBookButton = document.getElementById('new-book');
 
 newBookButton.onclick = openModal;
 
-const submitBookButton = document.querySelector('.submit');
-console.log(submitBookButton);
+const submitBookButton = document.querySelector('.submit')
 
 submitBookButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -22,32 +21,61 @@ function Book(title, author, genre, pages, read) {
     this.read = read
 }
 
+Book.prototype.toggleRead = (card, i) => {
+    i.read === 'yes'? i.read = 'no': i.read = 'yes';
+    const readP = card.querySelector('.read-p');
+    readP.innerHTML = `Have I read it?: ${i.read}`;
+}
+
 function addBookToLibrary(...details) {
     myLibrary.push(new Book(...details));
 }
 
 function createLibraryCards() {
-    for (let i = myLibrary.length-1; i >= 0; i--) {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'library-card');
-        let book = myLibrary[i];
-        let title = document.createElement('h2');
-        title.innerHTML = book['title'];
-        let author = document.createElement('p');
-        author.innerHTML = `By ${book['author']}`;
-        let genre = document.createElement('p');
-        genre.innerHTML = `Genre: ${book['genre']}`;
-        let pages = document.createElement('p');
-        pages.innerHTML = `Pages: ${book['pages']}`;
-        let read = document.createElement('p');
-        read.innerHTML = `Have I read it?: ${book['read']}`;
-        div.appendChild(title);
-        div.appendChild(author);
-        div.appendChild(genre);
-        div.appendChild(pages);
-        div.appendChild(read)
-        cards.appendChild(div);
+    let i = myLibrary.length-1;
+    let div = document.createElement('div');
+    div.classList.add('library-card', readStatus(i));
+    div.setAttribute('data-index', `${i}`);
+    let book = myLibrary[i];
+    let title = document.createElement('h2');
+    title.innerHTML = book['title'];
+    let author = document.createElement('p');
+    author.innerHTML = `By ${book['author']}`;
+    let genre = document.createElement('p');
+    genre.innerHTML = `Genre: ${book['genre']}`;
+    let pages = document.createElement('p');
+    pages.innerHTML = `Pages: ${book['pages']}`;
+    let read = document.createElement('p');
+    read.setAttribute('class', 'read-p');
+    read.innerHTML = `Have I read it?: ${book['read']}`;
+    let removeBtn = document.createElement('button');
+    removeBtn.setAttribute('class', 'rmv-book-btn');
+    removeBtn.addEventListener('click', removeBook);
+    removeBtn.innerHTML = 'Remove';
+    let toggleBtn = document.createElement('button');
+    toggleBtn.addEventListener('click', (e) => {
+        const card = e.target.closest('.library-card');
+        const i = myLibrary[card.dataset.index];
+        i.toggleRead(card, i);
+    });
+    toggleBtn.innerHTML = 'Read toggle';
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(genre);
+    div.appendChild(pages);
+    div.appendChild(read);
+    div.appendChild(toggleBtn);
+    div.appendChild(removeBtn);
+    cards.appendChild(div);
+}
+
+function readStatus(i) {
+    if (myLibrary[i].read === 'yes') {
+        return 'haveread';
+    } else {
+        return 'notread';
     }
+
 }
 
 function resetCardGrid() {
@@ -57,8 +85,13 @@ function resetCardGrid() {
     });
 }
 
+function removeBook() {
+    const card = this.closest('.library-card');
+    let i = card.dataset.index;
+    myLibrary.splice(i, 1);
+    card.remove();
 
-createLibraryCards();
+}
 
 function openModal() {
     let background = document.getElementById('modal-background');
@@ -74,11 +107,6 @@ function closeModal() {
     content.style.display = 'none';
 }
 
-// function stopPageRefresh(e) {
-//     e.preventDefault();
-//     closeModal();
-// }
-
 function submitNewBook() {
     const details = [];
     const inputs = document.getElementsByTagName('input');
@@ -93,11 +121,10 @@ function submitNewBook() {
     for (const btn of radioBtns) {
         if (btn.checked) {
             details.push(btn.value);
-            console.log(btn);
         }
     }
     addBookToLibrary(...details);
-    resetCardGrid();
+    
     createLibraryCards();
     closeModal();
     resetForm();
